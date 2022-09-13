dd2caa7f62c5fd8eed3839522fbd51d1f11afa13
Gabriel <g.mayta.trigozo@accenture.com>
2022-09-13 17:35:10 +0200

    integrated eslint, prettier and typescript rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import './App.scss'
+import './App.scss';
 
 function App() {
   return (
     <div>
-      <h1>Create React Vite App</h1>        
+      <h1>Create React Vite App</h1>
+      <p>aaa</p>
     </div>
   );
 }
